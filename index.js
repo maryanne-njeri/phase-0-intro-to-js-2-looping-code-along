17bac9c61f20c8f20e461449a142ff150a98fde9
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-06-13 15:33:07 +0300

    commit tree bda910b5a6e3ed9e8843e47aff48057cb6bf0d44

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+}
+
+return gifts;
+}
+
+wrapGifts(gifts);
+
+
+function writeCards(name, extr) {
+
+    let emp = [];
+    for(let i = 0; i < name.length; i++){
+        emp.push(`Thank you, ${name[i]}, for the wonderful ${extr} gift!`);
+        // console.log(emp);
+    }
+    return emp; 
+}
+
+function countDown(count) {
+    for (let j = count.length; j > 0; j--) {
+        console.log(count);
+        console.log(j +' '+ count);
+    }
+}
+
+// writeCards(nameArr);
